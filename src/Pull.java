4b8181475908c91a5f00ed8467598a9f78b58fd1
Fatih <fatih_ok4@hotmail.com>
2022-08-17 09:43:17 +0200

    eerste

---
+++ src/Pull.java
@@ -0,0 +1,31 @@
+public class Pull {
+     /*
+          /*
+        Git init bir kereye mahsus kullanırız local repo oluşturmak
+        için .git ile klasörümüzün içindeki dosyalarımızı illişkilendirmek
+        için kullanılır
+        2) git add . working space den  yerelden dosyalarımı staging areayı
+        commitlemek için beklenen yere gönderir
+        3) git commit -m "mesaj" staging area dan commit store a dosyalarımı
+        göndermek için kullanılır
+        commit yani versiyon oluşturmuş olurum
+        git log --oneline en son yaptığım commiti gösterir
+        4) git push remote yani uzak repo olan githuba  göndermek için kullandığımız kod
+        yanlız git push komutunu direkt kullanmak istersek 1 kez
+        git remote add origin -address bu adres github repost oluşturuduğumuz adres
+        git push -u origin master
+        Not: bu iki komutu tek seferde kullandıktan sonra diğer ikinci commitlerim için
+        sadece git push ykullanırım
+        Bir kez add komutunu kullandıktan sonra working stagede yaptığımız güncellemeler sonası
+        tekrar git add . gerek yok onun yerine
+        git  commit -a -m"" kodunu girebiliriz
+        Not: git log --oneline commitlerimin son durumunu gösterir
+         */
+     public static void main(String[] args) {
+         System.out.println("fatih");
+         System.out.println("Github is so painful");
+                 ;
+     };
+
+}
+
